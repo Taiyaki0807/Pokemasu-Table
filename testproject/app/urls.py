44f7f738eb0edad98c7048966bf1c24989f135b3
unknown <taiseipkd@outlook.jp>
2026-04-24 13:46:48 +0900

    第一

--- testproject/app/urls.py
+++ testproject/app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="home_page"),
+    path("third/", views.third, name="third_page")
 ]
--- testproject/app/views.py
+++ testproject/app/views.py
@@ -2,4 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
  
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "app/home.html")
+
+def third(request):
+    exn_hp = '22,170,000'
+    leaders = ['ビオラ', 'ザクロ', 'コル二', 'フクジ', 'シトロン', 'マーシュ', 'ゴジカ', 'ウルップ']
+    round_data = [
+        {'hp': '500,000', 'name': '1'},
+        {'hp': '3,460,000', 'name': '2'},
+        {'hp': '5,540,000', 'name': '3'},
+        {'hp': '7,980,000', 'name': 'Ex1'},
+        {'hp': '10,860,000', 'name': 'Ex2'},
+        {'hp': '14,190,000', 'name': 'Ex3'},
+        {'hp': '17,960,000', 'name': 'Ex4'},
+        {'hp': '22,170,000', 'name': 'Ex5'},
+    ]
+    for i in range(6, 21):
+        round_data.append({'hp': exn_hp, 'name': f'Ex{i}'})
+    return render(request, "app/third.html", {"rounds": round_data, "leaders": leaders})
